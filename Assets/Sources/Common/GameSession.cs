3d2ca77bfef6622e57b01724254309765d3f6521
pavel557 <Pawel.majer20@yandex.ru>
2023-02-01 17:02:53 +0300

    UI

--- Assets/Sources/Common/GameSession.cs
+++ Assets/Sources/Common/GameSession.cs
@@ -8,9 +8,17 @@ public class GameSession : Singleton<GameSession>
     public int Money;
 
     public int MaxAmountEnergy;
+    public int CurrentAmountEnergy;
     public int AmountFatigue;
 
     public int SumPullups;
     public int MaxPillups;
     public int MaxNumberRepetitions;
+
+    public void ChangeCurrentAmountEnergy(int value)
+    {
+        CurrentAmountEnergy += value;
+        Mathf.Clamp(CurrentAmountEnergy, 0, MaxAmountEnergy);
+        Debug.Log(CurrentAmountEnergy);
+    }
 }
